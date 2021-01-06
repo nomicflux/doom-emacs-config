203282c875fdb389b33b0ed78886be067ae5365e
Michael Anderson <manderson@drwholdings.com>
2021-01-06 13:55:09 -0600

    Basic config

---
+++ custom.el
@@ -0,0 +1,52 @@
+;;; custom.el ---                                    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Michael Anderson
+
+;; Author: Michael Anderson <manderson@DRWHoldings.com>
+;; Keywords:
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#28323e" "#FF8080" "#95FFA4" "#FFE9AA" "#91DDFF" "#C991E1" "#AAFFE4" "#CBE3E7"])
+ '(custom-safe-themes
+   (quote
+    ("990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" "188fed85e53a774ae62e09ec95d58bb8f54932b3fd77223101d036e3564f9206" "711efe8b1233f2cf52f338fd7f15ce11c836d0b6240a18fffffc2cbd5bfe61b0" "a3b6a3708c6692674196266aad1cb19188a6da7b4f961e1369a68f06577afa16" "3df5335c36b40e417fec0392532c1b82b79114a05d5ade62cfe3de63a59bc5c6" default)))
+ '(fci-rule-color "#858FA5")
+ '(jdee-db-active-breakpoint-face-colors (cons "#100E23" "#906CFF"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#100E23" "#95FFA4"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#100E23" "#565575"))
+ '(objed-cursor-color "#FF8080")
+ '(pdf-view-midnight-colors (cons "#CBE3E7" "#1E1C31"))
+ '(rustic-ansi-faces
+   ["#1E1C31" "#FF8080" "#95FFA4" "#FFE9AA" "#91DDFF" "#C991E1" "#AAFFE4" "#CBE3E7"])
+ '(vc-annotate-background "#1E1C31")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#95FFA4")
+    (cons 40 "#b8f7a6")
+    (cons 60 "#dbf0a8")
+    (cons 80 "#FFE9AA")
+    (cons 100 "#ffd799")
+    (cons 120 "#ffc488")
+    (cons 140 "#FFB378")
+    (cons 160 "#eda79b")
+    (cons 180 "#db9cbd")
+    (cons 200 "#C991E1")
+    (cons 220 "#db8bc0")
+    (cons 240 "#ed85a0")
+    (cons 260 "#FF8080")
+    (cons 280 "#d4757d")
+    (cons 300 "#aa6a7a")
+    (cons 320 "#805f77")
+    (cons 340 "#858FA5")
+    (cons 360 "#858FA5")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
