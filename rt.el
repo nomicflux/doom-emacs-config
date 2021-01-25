9b65b69c1b5750621cf687838a19e66509c7cf46
Michael Anderson <manderson@drwholdings.com>
2021-01-25 16:39:01 -0600

    More control over random themes

--- rt.el
+++ rt.el
@@ -1,23 +1,52 @@
 ;;; $DOOMDIR/rt.el -*- lexical-binding: t; -*-
 ;;;
 
-(require 'seq)
 (require 'doom-themes)
 
-(defun doom-theme-p (theme-name)
-  (string-prefix-p "doom-" (symbol-name theme-name)))
+(setq light-themes '(doom-flatwhite
+                     doom-gruvbox-light
+                     doom-nord-light
+                     doom-opera-light
+                     doom-solarized-light
+                     tao-yang))
 
-(defun random-doom-theme ()
+(setq dark-themes '(doom-Iosevkm
+                    doom-fairy-floss
+                    doom-gruvbox
+                    doom-molokai
+                    doom-monokai-spectrum
+                    doom-moonlight
+                    doom-nord
+                    doom-nova
+                    doom-oceanic-next
+                    doom-solarized-dark
+                    doom-spacegrey
+                    doom-wilmersdorf
+                    doom-zenburn
+                    tao-yin))
+
+(defun random-doom-theme (&optional themes)
   (interactive)
-  (let* ((doom-themes (seq-filter 'doom-theme-p custom-known-themes))
-         (num-themes (length doom-themes))
-         (n (random num-themes))
-         (theme (nth n doom-themes)))
+  (let* ((ts (if themes themes (append dark-themes light-themes)))
+         (n (random (length ts)))
+         (theme (nth n ts)))
     (progn
       (disable-theme doom-theme)
       (load-theme theme t))))
 
+(defun random-light-theme ()
+  (interactive)
+  (random-doom-theme light-themes))
+
+(defun random-dark-theme ()
+  (interactive)
+  (random-doom-theme dark-themes))
+
 (defun add-themes ()
   (map! :leader
         (:prefix "t"
-         :desc "Random Doom theme" "t" 'random-doom-theme)))
+         :desc "Random Doom theme" "s" 'random-doom-theme
+         :desc "Random Dark Doom theme" "t" 'random-dark-theme
+         :desc "Random Light Doom theme" "T" 'random-light-theme)))
+
+(load-theme 'doom-nord)
