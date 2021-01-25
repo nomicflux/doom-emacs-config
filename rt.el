6a21a319fe3b0bc1ef910e8a9973f11651257719
Michael Anderson <manderson@drwholdings.com>
2021-01-25 17:40:23 -0600

    Working on random theme change mode

--- rt.el
+++ rt.el
@@ -3,50 +3,69 @@
 
 (require 'doom-themes)
 
-(setq light-themes '(doom-flatwhite
-                     doom-gruvbox-light
-                     doom-nord-light
-                     doom-opera-light
-                     doom-solarized-light
-                     tao-yang))
+(setq rt-light-themes '(doom-flatwhite
+                        doom-gruvbox-light
+                        doom-nord-light
+                        doom-opera-light
+                        doom-solarized-light
+                        tao-yang))
 
-(setq dark-themes '(doom-Iosevkm
-                    doom-fairy-floss
-                    doom-gruvbox
-                    doom-molokai
-                    doom-monokai-spectrum
-                    doom-moonlight
-                    doom-nord
-                    doom-nova
-                    doom-oceanic-next
-                    doom-solarized-dark
-                    doom-spacegrey
-                    doom-wilmersdorf
-                    doom-zenburn
-                    tao-yin))
+(setq rt-dark-themes '(doom-fairy-floss
+                       doom-gruvbox
+                       doom-molokai
+                       doom-monokai-spectrum
+                       doom-moonlight
+                       doom-nord
+                       doom-nova
+                       doom-oceanic-next
+                       doom-solarized-dark
+                       doom-spacegrey
+                       doom-wilmersdorf
+                       doom-zenburn
+                       tao-yin))
 
-(defun random-doom-theme (&optional themes)
+(defun rt-random-doom-theme (&optional themes)
   (interactive)
-  (let* ((ts (if themes themes (append dark-themes light-themes)))
+  (let* ((ts (or themes (append rt-dark-themes rt-light-themes)))
          (n (random (length ts)))
          (theme (nth n ts)))
     (progn
       (disable-theme doom-theme)
       (load-theme theme t))))
 
-(defun random-light-theme ()
+(defun rt-random-light-theme ()
   (interactive)
-  (random-doom-theme light-themes))
+  (rt-random-doom-theme rt-light-themes))
 
-(defun random-dark-theme ()
+(defun rt-random-dark-theme ()
   (interactive)
-  (random-doom-theme dark-themes))
+  (rt-random-doom-theme rt-dark-themes))
 
 (defun add-themes ()
   (map! :leader
         (:prefix "t"
-         :desc "Random Doom theme" "s" 'random-doom-theme
-         :desc "Random Dark Doom theme" "t" 'random-dark-theme
-         :desc "Random Light Doom theme" "T" 'random-light-theme)))
+         :desc "Random Doom theme" "s" 'rt-random-doom-theme
+         :desc "Random Dark Doom theme" "t" 'rt-random-dark-theme
+         :desc "Random Light Doom theme" "T" 'rt-random-light-theme)))
 
-(load-theme 'doom-nord)
+(setq rt-random-theme-change-freq (* 5 1))
+(setq rt-random-theme-change-timer nil)
+
+(defun rt-randomly-change-theme ()
+  (when (bound-and-true-p rt-random-theme-change-mode)
+    (rt-random-doom-theme))
+  (if (bound-and-true-p rt-random-theme-change-mode)
+      (setq rt-random-theme-change-timer
+            (run-with-timer rt-random-theme-change-freq nil 'rt-randomly-change-theme))
+    (when rt-random-theme-change-timer
+      (cancel-timer rt-random-theme-change-timer))))
+
+(define-minor-mode rt-random-theme-change-mode
+  "Randomly change themes at a set interval"
+  :lighter " rt"
+  :global t)
+  ;; :after-hook 'randomly-change-theme)
+
+(add-hook 'rt-random-theme-change-mode-hook 'rt-randomly-change-theme)
+
+;; (rt-randomly-change-theme*)
