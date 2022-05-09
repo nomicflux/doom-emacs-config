6725eeee4e1f1eaf51137ff8494237d6377e75bb
Michael Anderson <manderson@drwholdings.com>
2022-05-09 14:07:44 -0500

    after init changes

--- config.el
+++ config.el
@@ -115,4 +115,20 @@
 
 (after! projectile 
   (setq projectile-project-root-files-bottom-up 
-    (remove ".git" projectile-project-root-files-bottom-up)))
+    (remove ".git" projectile-project-root-files-bottom-up))
+  (add-hook 'projectile-after-switch-project-hook (lambda ()
+      (projectile-invalidate-cache nil)))
+  
+  (add-hook 'after-init-hook (lambda ()
+    (mapc (lambda (project-root)
+        (remhash project-root projectile-project-type-cache)
+        (remhash project-root projectile-projects-cache)
+        (remhash project-root projectile-projects-cache-time)
+        (when projectile-verbose
+            (message "Invalidated Projectile cache for %s."
+                (propertize project-root 'face 'font-lock-keyword-face)))
+        (when (fboundp 'recentf-cleanup)
+            (recentf-cleanup)))
+        (projectile-hash-keys projectile-projects-cache))
+    (projectile-serialize-cache))))
+
